886307feb95be83d487d96766b4927eb2e519594
a.konoplev <a.konoplev@CMDB-123576>
2023-04-07 18:15:40 +0400

    fix

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice
+{
+    class Program
+    {
+        static void Main(string []args)
+        {
+            int a,b,c;
+            a = 30;
+            b = 60;
+            c = b--;
+            c = a >= 100 ? b : c / 10;
+            Console.Write($"{c}");
+            Console.ReadKey();
+        }
+    }
+}
